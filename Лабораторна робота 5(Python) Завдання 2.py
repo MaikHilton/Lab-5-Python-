eadd70223123a44623594304a7df97567ad0c337
MaikHilton <150069012+MaikHilton@users.noreply.github.com>
2024-09-19 15:20:05 +0300

    Update Лабораторна робота 5(Python) Завдання 2.py

--- Лабораторна робота 5(Python) Завдання 2.py
+++ Лабораторна робота 5(Python) Завдання 2.py
@@ -1,14 +1,1 @@
-size = 7
-array = [[1 for k in range(size)] for k in range(size)]
 
-for i in range(size):
-    if i <= 3:
-        for j in range(i + 1):
-            array[i][j] = 0
-    else:
-        for j in range(7 - i):
-            array[i][j] = 0
-
-
-for row in array:
-    print(row)
